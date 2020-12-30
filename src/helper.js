7a52d8be842d9b3a943a2a3e843223ba3b0cd7b3
bronwyncarr <bronwynacarr@gmail.com>
2020-12-30 13:05:46 +1100

    helper added to calculate winnner

---
+++ src/helper.js
@@ -0,0 +1,22 @@
+// Calculate winner function from React docs
+
+export function calculateWinner(squares) {
+  // Lookup array includes all winning combinations
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
